adbb24e75b00c6ddb4ebdea6d492fd87bcb55385
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-09-13 15:19:20 -0400

    Update Atividade7.java

--- Atividade7.java
+++ Atividade7.java
@@ -1,1 +1,27 @@
+package br.com.seunome.lista02;
 
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int hora;
+
+        System.out.print("Digite a hora do dia: ");
+        hora = entrada.nextInt();
+
+        if (hora >= 0 && hora <= 11) {
+            System.out.println("Bom dia!");
+        } else if (hora >= 12 && hora <= 17) {
+            System.out.println("Boa tarde!");
+        } else if (hora >= 18 && hora <= 23) {
+            System.out.println("Boa noite!");
+        } else {
+            System.out.println("Hora inválida.");
+        }
+
+    }
+}
