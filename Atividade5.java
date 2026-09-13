9d5c9591e97a66bbf8c035db07783763f7882117
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-09-13 15:18:52 -0400

    Update Atividade5.java

--- Atividade5.java
+++ Atividade5.java
@@ -1,1 +1,23 @@
+package br.com.seunome.lista02;
 
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int ano;
+
+        System.out.print("Digite um ano: ");
+        ano = entrada.nextInt();
+
+        if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+            System.out.println("O ano é bissexto.");
+        } else {
+            System.out.println("O ano não é bissexto.");
+        }
+
+    }
+}
