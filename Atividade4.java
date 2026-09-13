edc291dd9e353db5f5f97bc96c147fe14aadc8c9
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-09-13 15:18:36 -0400

    Implement multiple check between two numbers
    Add a program to check if one number is a multiple of another.

--- Atividade4.java
+++ Atividade4.java
@@ -1,1 +1,29 @@
+package br.com.seunome.lista02;
 
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero1;
+        int numero2;
+
+        System.out.print("Digite o primeiro número: ");
+        numero1 = entrada.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        numero2 = entrada.nextInt();
+
+        if (numero2 == 0) {
+            System.out.println("A verificação não pode ser feita.");
+        } else if (numero1 % numero2 == 0) {
+            System.out.println("O primeiro número é múltiplo do segundo.");
+        } else {
+            System.out.println("O primeiro número não é múltiplo do segundo.");
+        }
+
+    }
+}
