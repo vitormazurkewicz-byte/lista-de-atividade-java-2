2598dd2746afd9eafe7c9e4fa9005e6606d37a2f
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-09-13 15:19:33 -0400

    Implement speed limit check with violation categories

--- Atividade8.java
+++ Atividade8.java
@@ -1,1 +1,40 @@
+package br.com.seunome.lista02;
 
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double limite;
+        double velocidade;
+        double percentual;
+
+        System.out.print("Digite a velocidade máxima da via: ");
+        limite = entrada.nextDouble();
+
+        System.out.print("Digite a velocidade do veículo: ");
+        velocidade = entrada.nextDouble();
+
+        if (limite <= 0) {
+            System.out.println("Limite inválido.");
+        } else if (velocidade <= limite) {
+            System.out.println("Dentro do limite. Não há multa.");
+        } else {
+
+            percentual = ((velocidade - limite) / limite) * 100;
+
+            if (percentual <= 20) {
+                System.out.println("Infração média.");
+            } else if (percentual <= 50) {
+                System.out.println("Infração grave.");
+            } else {
+                System.out.println("Infração gravíssima.");
+            }
+
+        }
+
+    }
+}
