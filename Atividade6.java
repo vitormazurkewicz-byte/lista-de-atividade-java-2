f8e58afaa5396e740145f1edaa3d01f6072a9521
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-09-13 15:19:06 -0400

    Add shipping cost calculation to purchase total

--- Atividade6.java
+++ Atividade6.java
@@ -1,1 +1,30 @@
+package br.com.seunome.lista02;
 
+import java.util.Scanner;
+
+public class Exercicio06 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double compra;
+        double frete;
+        double total;
+
+        System.out.print("Digite o valor da compra: ");
+        compra = entrada.nextDouble();
+
+        if (compra >= 199.00) {
+            frete = 0.00;
+        } else {
+            frete = 24.90;
+        }
+
+        total = compra + frete;
+
+        System.out.printf("Valor do frete: R$ %.2f%n", frete);
+        System.out.printf("Total a pagar: R$ %.2f%n", total);
+
+    }
+}
