d0ccfeffe0d1b0bc815c46dc34d5331020918aef
vitormazurkewicz-byte <vitormazurkewicz@gmail.com>
2026-09-13 15:20:10 -0400

    Update Atividade10.java

--- Atividade10.java
+++ Atividade10.java
@@ -1,1 +1,33 @@
+package br.com.seunome.lista02;
 
+import java.util.Scanner;
+
+public class Exercicio10 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        String usuario;
+        String senha;
+
+        System.out.print("Digite o usuário: ");
+        usuario = entrada.nextLine();
+
+        if (usuario.equals("admin")) {
+
+            System.out.print("Digite a senha: ");
+            senha = entrada.nextLine();
+
+            if (senha.equals("java123")) {
+                System.out.println("Acesso liberado.");
+            } else {
+                System.out.println("Senha incorreta.");
+            }
+
+        } else {
+            System.out.println("Usuário não encontrado.");
+        }
+
+    }
+}
